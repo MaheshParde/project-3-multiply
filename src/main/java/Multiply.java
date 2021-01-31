725048141967ef820fdeb768a17ce304972a0b84
LAPTOP-58L05II1\parde <pardemahesh9595@gmai.com>
2021-01-31 23:48:54 +0530

    first commit

---
+++ src/main/java/Multiply.java
@@ -0,0 +1,7 @@
+public class Multiply {
+    public static void main(String[] args){
+        int num1=10,num2=20,mul;
+        mul=num1*num2;
+        System.out.println("Multiply of two numbers:"+mul);
+    }
+}
