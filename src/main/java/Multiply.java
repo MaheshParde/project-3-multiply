556459202fcca5f91f7ce2e6848456cf92e17a5b
MaheshParde <75496568+MaheshParde@users.noreply.github.com>
2022-02-28 09:32:10 +0530

    Update Multiply.java

--- src/main/java/Multiply.java
+++ src/main/java/Multiply.java
@@ -3,5 +3,6 @@ public class Multiply {
         int num1=10,num2=20,mul;
         mul=num1*num2;
         System.out.println("Multiply of two number:"+mul);
+        System.out.println("Multiply of two number:"+mul);
     }
 }
